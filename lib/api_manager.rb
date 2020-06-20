17cbdbef26d12d37c97511a192d51d9e6a0f62ba
billy-ferrara <willfira717@gmail.com>
2020-06-20 17:02:28 -0400

    gets data from api

--- lib/api_manager.rb
+++ lib/api_manager.rb
@@ -2,8 +2,12 @@ class Adventurer::APIManager
 
   BASE_URL = "https://www.dnd5eapi.co/api/"
 
-  def self.get_adventurer
+  def self.get_races
     puts "MAKING A NETWORK REQUEST"
+
+    url = BASE_URL + "races"
+    res = HTTParty.get(url)
+    res 
   end
 
 

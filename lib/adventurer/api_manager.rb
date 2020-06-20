5b4de4bb2c769919a14fa8e67cd6e521694ca3db
billy-ferrara <willfira717@gmail.com>
2020-06-20 15:11:54 -0400

    2nd commit

---
+++ lib/adventurer/api_manager.rb
@@ -0,0 +1,11 @@
+class Adventurer::APIManager
+
+  BASE_URL = "https://www.dnd5eapi.co/api/"
+
+  def self.get_adventurer
+    puts "MAKING A NETWORK REQUEST"
+  end
+
+
+
+end
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,10 @@
-require "adventurer/version"
+require_relative "./adventurer/version"
+require 'pry'
+require 'httparty'
+
+require_relative './api_manager'
+require_relative './run'
+
 
 module Adventurer
   class Error < StandardError; end
